342c9ea886bb766dd462376060b06d3ef72b33d7
jaiganesh0312 <jgtumkar@gmail.com>
2026-01-05 14:23:15 +0530

    Upgrade tailwind to v4 and HeroUI to v2.8.7

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,17 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
+import tailwindcss from '@tailwindcss/vite'
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig(() => {
   return {
-    server: {
-      allowedHosts: ['c8b85f836e5a.ngrok-free.app'],
-    },
     plugins: [
       react(),
+      tailwindcss(),
     ],
     resolve: {
       alias: {
